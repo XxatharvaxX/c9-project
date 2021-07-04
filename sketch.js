bd02845388310ac7117c1637e5946f50a4a104cf
XxatharvaxX <86345529+XxatharvaxX@users.noreply.github.com>
2021-07-04 19:09:23 +1200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,32 @@
 var btn_red;
 var btn_green;
+var btn_blue;
 
 var r = 0;
 var g = 0;
 var b = 0;
 
 function setup() {
-  createCanvas(400, 400);  
+  createCanvas(400, 400); 
 
-btn_red.createButton("RED");
+btn_red=createButton("RED");
 btn_red.position(100,50);
 btn_red.mousePressed(red_bg);
 
-btn_green.createButton("GREEN");
+btn_green=createButton("GREEN");
 btn_green.position(250,50);
 btn_green.mousePressed(green_bg);
+
+btn_blue=createButton("BLUE");
+btn_blue.position(100,200);
+btn_blue.mousePressed(blue_bg);
 
 function red_bg()
 {
   r = 255;
   g = 0;
   b = 0;
+  y = 0;
 }
 
 function green_bg()
@@ -28,6 +34,15 @@ function green_bg()
   r = 0;
   g = 255;
   b = 0;
+  y = 0;
+}
+
+function blue_bg()
+{
+  r = 0;
+  g = 0;
+  b = 255;
+  y = 0;
 }
 
 }
@@ -36,5 +51,7 @@ function green_bg()
 
 function draw() {
   background(r,g,b);
+
+  console.log(r,g,b);
 }
 
